26c0610ce5b1c6cef618268bcbc9fc0f2c11afe1
SemafonKA <s_se@ngs.ru>
2020-02-20 15:03:44 +0700

    Const дописал

--- main.cpp
+++ main.cpp
@@ -157,10 +157,10 @@ public:
 	}
 
 	/* Возвращает размер списка */
-	unsigned int size() { return m_listSize; }
+	unsigned int size() const { return m_listSize; }
 
 	/* Возвращает, пустой ли список или нет */
-	bool isEmpty() { return m_listSize == 0; }
+	bool isEmpty() const { return m_listSize == 0; }
 
 	/* Добавляет новое звено в начало списка */
 	List& push_front(int date = 0) { return push(-1, date); }
@@ -169,10 +169,10 @@ public:
 	List& push_back(int date = 0) { return push(m_listSize - 1, date); }
 
 	/* Возвращает значение в последнем звене списка */
-	int back() { return pos_back(m_listSize - 1); }
+	int back() const { return pos_back(m_listSize - 1); }
 
 	/* Возвращает значение в первом звене списка */
-	int front() { return pos_back(0); }
+	int front() const { return pos_back(0); }
 
 	/* Удаляет последнее звено списка */
 	int pop_back() { return remove(m_listSize - 1); }
