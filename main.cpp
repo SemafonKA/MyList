168c2f66a44528a9c6ab56abc58828e6a49f3717
SemafonKA <s_se@ngs.ru>
2020-02-20 15:16:36 +0700

    Кодировка вывода
    Кодировка вывода на chcp 65001

--- main.cpp
+++ main.cpp
@@ -182,7 +182,7 @@ public:
 };
 
 int main() {
-	system("chcp 1251"); system("cls");
+	system("chcp 65001"); system("cls");
 
 	List list;
 	constexpr auto SIZE = 1'000;
@@ -198,7 +198,6 @@ int main() {
 	/*
 		Просто убиваем память проекта, ~3 ГБ памяти, причём дважды
 	*/
-
 	constexpr auto SIZE2 = 40'000'000;
 	constexpr auto SIZE3 = 2'000'000;
 	cout << "All: ";
@@ -214,7 +213,7 @@ int main() {
 	cout << endl;
 	list.clear();
 
-	cout << "All: ";
+	/*cout << "All: ";
 	for (int i = 0; i < SIZE2; i += SIZE3) {
 		cout << "[  ]";
 	}
@@ -225,9 +224,9 @@ int main() {
 		if (i % SIZE3 == 0) cout << " || ";
 	}
 	cout << endl;
-	list.clear();
+	list.clear();*/
 
-	if (list.isEmpty()) cout << "Лист пустой!";
+	if (list.isEmpty()) cout << "Лист пустой!!";
 	else cout << "Лист не пустой((";
 	cout << endl;
 
